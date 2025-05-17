c35182b6e1c3874e34d17a8c34e865336006868a
Matteo Sbarluzzi <matteosbarluzzi1@gmail.com>
2025-05-17 14:16:25 +0200

    User story 9 completata

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use Spatie\Image\Image;
 use Spatie\Image\Enums\CropPosition;
+use Spatie\Image\Enums\Unit;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -11,31 +12,43 @@ class ResizeImage implements ShouldQueue
 {
     use Queueable;
 
-    //private accessibili solo all'interno della classe
-    //memorizzano rispettivamente width, height, nome file di immagine salvata, percorso directory immagine 
+    // private: accessibili solo all'interno della classe memorizzano rispettivamente width, height, nome file immagine salvata, percorso directory immagine 
     private $w, $h, $fileName, $path;
-    //funzione costruttore richiamata ogni volta che viene creata una nuova istanza della classe
-    public function __construct($filePath, $w, $h)//percorso, larghezza e altezza desiderati per manipolazione immagine
+
+    // funzione costruttore richiamata ogni volta che viene creata una nuova istanza della classe
+    public function __construct($filePath, $w, $h) // percorso, larghezza e altezza desiderati per manipolazione immagine
     {
-        $this->path = dirname($filePath); //estrae il percorso della directory estratto dall'argomento $filePath
-        $this->fileName = basename($filePath); //estrae il nome del file...
-        $this->w = $w; //assegna valore dell'argomento $w alla proprietà $w della classe
+        $this->path = dirname($filePath); // estrae il percorso della directory dal path completo
+        $this->fileName = basename($filePath); // estrae il nome del file con estensione
+        $this->w = $w; // assegna valore dell'argomento $w alla proprietà $w della classe
         $this->h = $h;
     }
 
-
     public function handle(): void
-    {   
-        //assegnano i valori delle proprieta $w e $h, memorizzando larghezza e altezza dell'immagine
+    {
+        // assegnano i valori delle proprietà $w e $h a variabili locali
         $w = $this->w;
         $h = $this->h;
-        //costruisce percorso completo del file immagine originale
+
+        // costruisce percorso completo del file immagine originale
         $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
-        //costruisce percorso completo di destinazione del file immagine ritagliata, simile al precedente, ma aggiunge il crop prima del nome del file originale per indicare dimensione ritaglio; le graffe attorno $w e $h consentono interpolazione variabili per creare dinamicamente nome file
+
+        // costruisce percorso completo di destinazione del file immagine ritagliata, aggiungendo 'crop_{w}x{h}_' al nome del file originale
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
-        Image::load($srcPath)//utilizza la facciata Image fornita dalla libreria Spatie/Image per caricare l'immagine originale dal percorso $srcPath
-            ->crop($w, $h, CropPosition::Center) //ritaglia immagine caricata
-            ->save($destPath); //salvataggio dell'immagine ritagliata nel percorso di destinazione
+        Image::load($srcPath) // utilizza la libreria Spatie Image per caricare l'immagine originale
+            ->crop($w, $h, CropPosition::Center) // ritaglia l'immagine centrata con larghezza e altezza desiderate
+
+            // aggiunge un watermark visibile sull'immagine croppata
+            ->watermark(
+                base_path('resources/img/prestowatermark.png'),
+                width: 100,       
+                height: 100,      
+                paddingX: 5,     // distanza orizzontale dal bordo (in percentuale)
+                paddingY: 5,     // distanza verticale dal bordo (in percentuale)
+                paddingUnit: Unit::Percent // specifica che paddingX e Y sono espressi in percentuale
+            )
+
+            ->save($destPath); // salvataggio dell'immagine modificata nel percorso di destinazione
     }
 }
